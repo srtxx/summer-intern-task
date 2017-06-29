15912ebb9e405e779dc0370e364f95e1dcd91b6c
Suganuma <b1504550@planet.kanazawa-it.ac.jp>
2017-06-29 15:15:07 +0900

    do [rails g controller][rails g model]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'post/index'
+
+  get 'post/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
